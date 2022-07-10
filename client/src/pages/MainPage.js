aba5fa0f147da20cdd5613d54d0d848815758152
Nick Ng <michaelvardy@Michaels-MacBook-Air.local>
2022-07-11 00:02:03 +0800

    feat: display lid status

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,5 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const App = () => <div>Rice Delivery App</div>;
+import useQuery from "hooks/useQuery";
+import { upperCaseFirstChar } from "utils/wordFormatter";
 
-export default App;
+import Spinner from "components/loader/Spinner";
+
+const MainPage = () => {
+  const [lid, setLid] = useState("");
+
+  const { data: status, isFetching: isStatusFetching } = useQuery({
+    method: "get",
+    endpoint: "/status",
+  });
+
+  useEffect(() => {
+    setLid(status?.lid);
+  }, [status]);
+
+  return (
+    <main>
+      {!isStatusFetching && status && lid && (
+        <>
+          <div className="bg-gray-50 p-4 rounded-lg">
+            <div>Lid Status: {upperCaseFirstChar(lid)}</div>
+          </div>
+        </>
+      )}
+
+      {isStatusFetching && (
+        <>
+          <main className="absolute inset-0 flex justify-center items-center">
+            <Spinner />
+          </main>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default MainPage;
---
+++ client/src/utils/wordFormatter.js
@@ -0,0 +1,5 @@
+const upperCaseFirstChar = (word) => {
+  return word?.charAt(0).toUpperCase() + word?.slice(1);
+};
+
+export { upperCaseFirstChar };
