3b6bb44d8a764748ef8e9aeaf884625f2daae8c4
Nick Ng <michaelvardy@Michaels-MacBook-Air.local>
2022-07-13 23:25:28 +0800

    feat: display error in modal

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import Spinner from "components/loader/Spinner";
 import Button from "components/button/Button";
+import Modal from "components/modal/ErrorModal";
 
 import { useGetStatusQuery, usePostLidMutation } from "services/robotApi";
 
@@ -9,12 +10,18 @@ import { upperCaseFirstChar } from "utils/wordFormatter";
 
 const MainPage = () => {
   const [lid, setLid] = useState("");
+  const [error, setError] = useState(null);
 
   const { data: status, isFetching: isStatusFetching } = useGetStatusQuery();
 
   const [lidToggle, { isLoading: isLidToggling }] = usePostLidMutation();
 
   const openLid = () => {
+    if (lid === "Open") {
+      setError("The lid is already opend.");
+      return;
+    }
+
     lidToggle({ lid: "Open" })
       .then(() => {
         setLid("Open");
@@ -25,6 +32,11 @@ const MainPage = () => {
   };
 
   const closeLid = () => {
+    if (lid === "Close") {
+      setError("The lid is already closed.");
+      return;
+    }
+
     lidToggle({ lid: "Close" })
       .then(() => {
         setLid("Close");
@@ -35,7 +47,7 @@ const MainPage = () => {
   };
 
   useEffect(() => {
-    setLid(status?.lid);
+    setLid(upperCaseFirstChar(status?.lid));
   }, [status]);
 
   return (
@@ -44,7 +56,7 @@ const MainPage = () => {
         <>
           <div className="space-y-4">
             <div className="bg-gray-50 p-4 rounded-lg">
-              <div>Lid Status: {upperCaseFirstChar(lid)}</div>
+              <div>Lid Status: {lid}</div>
             </div>
             <Button text="Open Lid" onClick={openLid} />
             <Button text="Close Lid" onClick={closeLid} />
@@ -63,6 +75,8 @@ const MainPage = () => {
           </main>
         </>
       )}
+
+      {error && <Modal error={error} setError={setError} />}
     </main>
   );
 };
